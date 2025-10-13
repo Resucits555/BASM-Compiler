e62f7bb6452a65fb3aa47fd223e1136713cadbd8
resic <170962741+Resucits555@users.noreply.github.com>
2025-10-13 16:29:29 +0200

    Argument interpretation updated

--- src/compilationData.h
+++ src/compilationData.h
@@ -1,6 +1,6 @@
 #pragma once
 
-//Abbreviations for type, addr and other in this code come from http://ref.x86asm.net/index.html#Instruction-Operand-Codes
+//Abbreviations in this code come from http://ref.x86asm.net/index.html#Instruction-Operand-Codes
 //The table found at that website was copied into an xml file, which is used here as an opcode reference.
 
 struct opcodeParts_x86 {
@@ -8,6 +8,7 @@ struct opcodeParts_x86 {
     ubyte so = 0;   //secondary
     sbyte o = -1;   //extension
 };
+
 
 union opcode_x86 {
     uint32_t full;
@@ -19,7 +20,7 @@ union opcode_x86 {
 
 struct argument {
     char type = 0;
-    char addr = 0;
+    ubyte size = 0;
     bool negative = false;
     uintmax_t val = 0;
 };
@@ -28,11 +29,12 @@ struct argument {
 
 
 struct instruction {
-    int8_t prefixes[4] = {};
+    std::vector<ubyte> prefixes = {};
     opcode_x86 opcode = {};
-    int8_t modrm = 0;
+    ubyte modrm = 0;
     bool modrmUsed = false;
-    int8_t sib = 0;
+    ubyte sib = 0;
     bool sibUsed = false;
-    argument arguments[2] = {};
+    ubyte dataSize = 0;
+    uintmax_t data = 0; //addresses and immediate values
 };
--- src/textCompiler.cpp
+++ src/textCompiler.cpp
@@ -6,6 +6,18 @@
 #include "compilationData.h"
 
 
+//Returns the minimum amount of bits the value could be stored in
+static ubyte minBitsToStore(uintmax_t value, bool negative) {
+    if (negative)
+        value = ~value << 1;
+
+    return floor(log2(value) + 1);
+}
+
+
+
+
+
 inline static sbyte NewSection(std::string& inputLine) {
     inputLine = inputLine.substr(1, SIZE_MAX);
 
@@ -16,7 +28,7 @@ inline static sbyte NewSection(std::string& inputLine) {
             }
         }
     }
-
+    
     return -1;
 }
 
@@ -24,32 +36,72 @@ inline static sbyte NewSection(std::string& inputLine) {
 
 
 
-inline static void GetArguments(std::string& inputLine, argument* args) {
-    mrx::substr instructionParts[4] = {};
-                         // mnem type1  arg1  type2  arg2
-    const char pat[] = "%0%s*%a+ %1%a?.?%2%w, %3%a?.?%4%w";
-    mrx::FindRgxSectional(instructionParts, inputLine, pat);
+static std::optional<argument> FindRegisterRef(const char* string) {
+    const char registers[][6] = {
+    "al", "cl", "dl", "bl", "ah", "ch", "dh", "bh", "r8l", "r9l", "r10l", "r11l", "r12l", "r13l", "r14l", "r15l",
+    "ax", "cx", "dx", "bx", "sp", "bp", "si", "di", "r8w", "r9w", "r10w", "r11w", "r12w", "r13w", "r14w", "r15w",
+    "eax", "ecx", "edx", "ebx", "esp", "ebp", "esi", "edi", "r8d", "r9d", "r10d", "r11d", "r12d", "r13d", "r14d", "r15d",
+    "rax", "rcx", "rdx", "rbx", "rsp", "rbp", "rsi", "rdi", "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15",
+    //"spl", "bpl", "sil", "dil"
+    /*"xmm0", "xmm1", "xmm2", "xmm3", "xmm4", "xmm5", "xmm6", "xmm7", "xmm8", "xmm9", "xmm10", "xmm11", "xmm12", "xmm13", "xmm14", "xmm15",
+    "ymm0", "ymm1", "ymm2", "ymm3", "ymm4", "ymm5", "ymm6", "ymm7", "ymm8", "ymm9", "ymm10", "ymm11", "ymm12", "ymm13", "ymm14", "ymm15",
+    "st0", "st1", "st2", "st3", "st4", "st5", "st6", "st7",
+    "mmx0", "mmx1", "mmx2", "mmx3", "mmx4", "mmx5", "mmx6", "mmx7"*/
+    };
+
+    for (ubyte stringI = 0; stringI < std::size(registers); stringI++) {
+        for (ubyte charI = 0; string[charI] == registers[stringI][charI]; charI++) {
+            if (registers[stringI][charI] == NULL) {
+                return argument('r', (ubyte)pow(2, stringI >> 4), false, (ubyte)(stringI % 16));
+            }
+        }
+    }
+    return std::nullopt;
+}
 
 
+
+
+inline static void GetArguments(const char* inputLine, argument* args, ulong line) {
+    char* tokenizedInput = (char*)malloc(strlen(inputLine) + 1);
+    strcpy(tokenizedInput, inputLine);
+
+    const char delimiters[] = " \t,.";
+    strtok(tokenizedInput, delimiters);
+
+    char* argstr = (char*)1;
+    for (sbyte i = 0; argstr != nullptr; i++) {
+        if (i >= 3)
+            Error("Invalid arguments", line);
+
+        argument& arg = args[i];
+        argstr = strtok(nullptr, delimiters);
+
+        if (!mrx::FindRgx(inputLine, "%D").has_value()) {
+            arg.type = 'I';
+            if (*argstr == '-')
+                arg.negative = true;
+            arg.val = std::stoull(argstr, nullptr, 0);
+        }
+        else {
+            std::optional<argument> reg = FindRegisterRef(argstr);
+            if (reg.has_value()) {
+                arg = reg.value();
+                return;
+            }
+        }
+    }
+
+    free(tokenizedInput);
 }
 
 
 
 
 
-//Returns the minimum amount of bits the value could be stored in
-static ubyte minBitsToStore (uintmax_t value, bool negative) {
-    if (value == 0)
-        return 1;
-    if (negative)
-        value = value << 1;
-
-    return floor(log2(value) + 1);
-}
+inline static instruction FindInstruction(const char* mnemonic, argument* args) {
 
 
-
-inline static instruction FindInstruction(const char* mnemonic) {
     return {};
 }
 
@@ -57,22 +109,17 @@ inline static instruction FindInstruction(const char* mnemonic) {
 
 
 
-inline static void WriteToExe(std::ofstream& exeFile, instruction op, argument* args) {
-    ubyte towrite = 0;
-    for (towrite; op.prefixes[towrite]; towrite++);
-    exeFile.write((char*)&op.prefixes, towrite);
+inline static void WriteToExe(std::ofstream& exeFile, instruction instr) {
+    for (ubyte prefix = 0; prefix < instr.prefixes.size(); prefix++)
+        exeFile.put(instr.prefixes[prefix]);
 
+    opcodeParts_x86& prs = instr.opcode.parts;
 
-    opcodeParts_x86& prs = op.opcode.parts;
-
-    exeFile.write((char*)&prs.po, 1);
+    exeFile.put(prs.po);
     if (prs.po == 0x0F)
-        exeFile.write((char*)&prs.so, 1);
+        exeFile.put(prs.so);
     
-
-    for (ubyte arg = 0; args[arg].type; arg++) {
-        exeFile.write((char*)&args[arg].val, 4);
-    }
+    exeFile.write((char*)&instr.data, instr.dataSize);
 }
 
 
@@ -111,21 +158,26 @@ inline void CompileSource(const fs::path& srcPath, std::ofstream& exeFile) {
                 Error("Unknown section name", line);
             else
                 section = newSection;
+            continue;
         }
-        
+
+
+        size_t commentPos = inputLine.find(';');
+        if (commentPos != std::string::npos)
+            inputLine = inputLine.substr(0, commentPos--);
 
         std::optional<const char*> mnemonic = mrx::FindRgxCSubstr(inputLine, "%a+");
 
         if (!mnemonic.has_value())
             Error("Invalid characters", line);
 
-        instruction instr = FindInstruction(mnemonic.value());
-
-
+        instruction instr;
         argument args[2];
-        GetArguments(inputLine, args);
+        GetArguments(inputLine.c_str(), args, line);
 
-        WriteToExe(exeFile, instr, args);
+        instr = FindInstruction(mnemonic.value(), args);
+
+        WriteToExe(exeFile, instr);
     }
 
     sourceFile.close();
